bb468cef15d89ba612aa3ada20264109a11f4df1
Marco Tulio R Braga <marco.tulio@mtulio.eng.br>
2016-09-16 10:03:31 -0300

    201609161003

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-res.send('Hello World! [201609160851]');
+res.send('Hello World! [201609161003]');
 });
 
 app.listen(3000, function () {
