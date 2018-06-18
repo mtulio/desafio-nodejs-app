2e63673824e01524a5d5f6274d8faa3eabfce099
Marco Tulio R Braga <marco.tulio@mtulio.eng.br>
2018-06-17 23:40:09 -0300

    Update to read cli args

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-res.send('Hello World! [201609161354]');
+var args_cli = {}
+var args = {}
+
+process.argv.forEach(function (val, index, array) {
+  if (index % 2 == 0) {
+    //console.log(index + ': ' + val);
+    args_cli[val] = process.argv[index+1]
+  }
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+args_app = {
+	"port": args_cli['--production.port'] || 3000,
+	"env": args_cli['--environment'] || 'dev'
+}
+
+app.get('/', function (req, res) {
+  //res.send('Hello World!', Date.now());
+  res.send('Hello World! ' + Date.now() || 0);
+});
+
+
+app.listen(args_app.port, function () {
+  console.log('Sample App using env ' + args_app.env + ' listening on port ', args_app.port);
 });
 
